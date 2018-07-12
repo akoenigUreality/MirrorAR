c93806ac12c3985ab83abe45a13da49cc11affd6
akoenig <akoenig@ureality.de>
2018-07-12 16:19:50 +0200

    spawn more precise, destroy on collision + vibrate, respawn at last one

---
+++ MirrorAR/Assets/Scripts/ColliderBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Thalmic.Myo;
+using UnityEngine;
+
+public class ColliderBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ManagerScript manager = GameObject.Find("Manager").GetComponent<ManagerScript>();
+        if (GameObject.FindGameObjectsWithTag("WayPoint").Length == 1)
+        {
+            manager.myo.GetComponent<ThalmicMyo>().Vibrate(VibrationType.Medium);
+            manager.SpawnRoute();
+        }
+        else
+        {
+            manager.myo.GetComponent<ThalmicMyo>().Vibrate(VibrationType.Short);
+        }
+        Debug.Log("collision");
+        Destroy(this.gameObject);
+    }
+    private void OnTriggerEnter()
+    {
+        
+    }
+}
--- MirrorAR/Assets/Scripts/ManagerScript.cs
+++ MirrorAR/Assets/Scripts/ManagerScript.cs
@@ -11,6 +11,7 @@ using VibrationType = Thalmic.Myo.VibrationType;
 public class ManagerScript : MonoBehaviour {
     public GameObject myo = null;
     public Text helpText;
+    public GameObject spawn;
 
     private bool started = false;
     private SkeletonRenderer skeletonViewer;
@@ -45,10 +46,10 @@ public class ManagerScript : MonoBehaviour {
         var rightEllbowPos = body.Joints[6].WorldPosition; //transform to local Position!!
         jointPositions[6] = new Vector3(rightEllbowPos.X / 1000f, rightEllbowPos.Y / 1000f, rightEllbowPos.Z / 1000f);
         var rightShoulderPos = body.Joints[5].WorldPosition; //transform to local Position!!
-        jointPositions[5] = new Vector3(rightEllbowPos.X / 1000f, rightEllbowPos.Y / 1000f, rightEllbowPos.Z / 1000f);
+        jointPositions[5] = new Vector3(rightShoulderPos.X / 1000f, rightShoulderPos.Y / 1000f, rightShoulderPos.Z / 1000f);
     }
 
-    void SpawnRoute() {
+   public void SpawnRoute() {
         //route gestreckter Arm, von nach unten hängen, gestreckt bis gerade nach oben
         //distance zwischen hand/ellenbogen/schulter soll gleich bleiben, anderer winkel! --> kein x movement?
         float distHandEllbow = Vector3.Distance(jointPositions[7], jointPositions[6]);
@@ -56,40 +57,29 @@ public class ManagerScript : MonoBehaviour {
         float distHandShoulder = Vector3.Distance(jointPositions[7], jointPositions[5]);
         Debug.Log("dist: distHandShoulder " + distHandShoulder + jointPositions[7].y + jointPositions[5].y);
 
-        //endpos = shoulder up + dist hand-shoulder bzw y wert ändern!
-        Vector3 endpos = jointPositions[5] + Vector3.up * 2 * distHandShoulder;   // Mytherious 2 Factor ... what ever!
-
-        //middlepos shoulder forward
-        Vector3 middlepos = jointPositions[5] - Vector3.forward * 2 * distHandShoulder;   // Mytherious 2 Factor ... what ever!
-
 
         //more positions:
-        Vector3[] positions = new Vector3[2];
-        positions[0] = jointPositions[5] - (Vector3.up + Vector3.forward) *  distHandShoulder  ;
-        positions[1] = jointPositions[5] + (Vector3.up - Vector3.forward) * distHandShoulder;
+        Vector3[] positions = new Vector3[5];
+       // positions[0] = jointPositions[5] - (Vector3.up + Vector3.forward) *  distHandEllbow  ;
+        //positions[1] = jointPositions[5] + (Vector3.up - Vector3.forward) * distHandEllbow;
+        positions[0] = new Vector3(jointPositions[5].x, jointPositions[7].y + distHandEllbow, jointPositions[7].z - distHandEllbow);
+        positions[1] = new Vector3(jointPositions[5].x, jointPositions[5].y + distHandEllbow, jointPositions[5].z - distHandEllbow);
+        positions[2] = new Vector3(jointPositions[5].x, jointPositions[5].y + distHandShoulder, jointPositions[7].z);
+        positions[3] = new Vector3(jointPositions[5].x, jointPositions[5].y, jointPositions[5].z - distHandShoulder);
+        positions[4] = new Vector3(jointPositions[5].x, jointPositions[7].y, jointPositions[7].z);
         //spawn cubes:
 
-
-        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.position = endpos;
-        cube.transform.localScale = new Vector3(.1f,.1f,.1f);
-
         
-        GameObject cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube2.transform.position = middlepos;
-        cube2.transform.localScale = new Vector3(.1f, .1f, .1f);
-
-        GameObject cube3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube3.transform.position = jointPositions[7];
-        cube3.transform.localScale = new Vector3(.1f, .1f, .1f);
-
         for (int i=0; i< positions.Length; i++)
         {
+            Instantiate(spawn, positions[i],Quaternion.identity);
+            /*
             GameObject cubes = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cubes.transform.position = positions[i];
             cubes.transform.localScale = new Vector3(.1f, .1f, .1f);
+            */
         }
-
+        
     
     }
 
